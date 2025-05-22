c2dd23d08ca577400f1994afb87f825c4c692eb5
RyanSaxe <ryancsaxe+1@gmail.com>
2025-05-22 17:18:03 -0400

    got to close to good state

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -13,18 +13,21 @@ return {
       hl["@string.documentation"] = { fg = Util.blend_bg(c.purple, 0.5) }
       -- Color shifts I prefer
       hl["@keyword.import"] = { fg = c.magenta }
-      hl["@type"] = { fg = c.yellow }
+      hl["@type"] = { fg = c.green1 }
       -- I prefer when the literals are the same color and dont pop out at me
-      local muted_literal = { fg = Util.blend_bg(c.blue6, 0.8) }
+      local muted_literal = { fg = Util.blend_bg(c.green1, 0.9) }
       hl["@string"] = muted_literal
       hl["@number"] = muted_literal
       hl["@number.float"] = muted_literal
       hl["@boolean"] = muted_literal
       -- builtins should match the normal color
-      hl["@function"] = { fg = c.green1 }
+      hl["@function"] = { fg = c.magenta }
       hl["@type.builtin"] = "@type"
-      hl["@function.builtin"] = "@function"
+      hl["@function.builtin"] = { fg = Util.blend_bg(c.cyan, 1) }
+      hl["@function.call"] = { fg = c.yellow }
+      hl["@function.method.call"] = { fg = c.yellow }
       hl["@constant.builtin"] = "@type"
+      hl["@constant"] = { fg = c.magenta2 }
       -- g = c.purple }
       hl["@keyword.conditional"] = { fg = c.purple }
       hl["@keyword.repeat"] = { fg = c.purple }
@@ -32,23 +35,23 @@ return {
       -- non-red for other keyword groups
       hl["@keyword.exception"] = { fg = c.purple }
       -- hl["@keyword"] = { fg = c.magenta2 }
-      hl["@keyword.risky"] = { fg = c.magenta2 }
+      hl["@keyword.risky"] = { fg = c.bg, bg = c.magenta2 }
       hl["@keyword.error"] = { fg = c.red }
       -- remove things where particular functions and definitions have slightly different coloring
       hl["@keyword.function"] = { fg = c.purple }
       hl["@keyword.return"] = { fg = c.purple }
       hl["@keyword.type"] = { fg = c.purple }
-      hl["@constructor"] = "@function"
+      hl["@constructor"] = { fg = c.orange }
       hl["@function.method"] = "@function"
       hl["@module"] = { fg = c.cyan }
       -- make self the same color as arguments ... I dont like it to jump out separately like that
       hl["@variable.builtin"] = { fg = c.magenta }
-      hl["@variable"] = { fg = c.blue }
+      hl["@variable"] = { fg = c.blue1 }
       hl["@variable.member"] = { fg = c.cyan }
       hl["@variable.parameter"] = { fg = c.cyan }
-      hl["@operator"] = { fg = c.magenta }
-      hl["@punctuation.delimiter"] = { fg = "#FFFFFF" }
-      hl["@punctuation.brackets"] = { fg = c.purple }
+      hl["@operator"] = { fg = c.cyan }
+      hl["@punctuation.delimiter"] = { fg = c.purple }
+      hl["@punctuation.bracket"] = { fg = c.purple }
     end,
   },
 }
